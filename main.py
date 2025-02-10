f13a15825c9591c74ea9cbde02e7e7243070ab3a
Jani Piirainen <piirainen.jani98@gmail.com>
2025-02-10 11:37:19 +0200

    Main valmis

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 import logic
 import ui
 
+def main():
+    print("Tervetuloa pelaamaan Yatzya!")
+    pelaajat = ui.kysy_pelaajat()
+    peli = logic.YatzyPeli(pelaajat)
+
+    while not peli.onko_peli_loppu():
+        ui.nayta_pelitilanne(peli)
+        peli.pelaa_vuorot()
+    
+    ui.nayta_voittaja(peli)
+
+if __name__ == "__main__":
+    main()
